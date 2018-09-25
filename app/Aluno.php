01df5c8563681d0def21e72003801eb93f934776
edierodrigues <edvaldoroddrigues@hotmail.com>
2018-09-24 21:26:28 -0300

    heroku configurado

---
+++ app/Aluno.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Aluno extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    protected $table = 'alunos';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'aluno_nome',
+        'aluno_cpf',
+        'aluno_estado',
+        'aluno_municipio',
+        'aluno_telefone',
+        'aluno_email',
+        'aluno_senha',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'aluno_senha',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->aluno_senha;
+    }
+}
